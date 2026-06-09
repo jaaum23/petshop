b35a73f50814b998eda1ef5730fa196389791a62
João Pedro (Macbook) <joaopedrofreitas23@gmail.com>
2026-06-08 23:42:28 -0300

    fix: adjust components with broken layout in mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Period } from "@/components/periods";
 export default function Home() {
   return (
     <main className="bg-background-primary font-inter text-content-primar min-h-screen w-full">
-      <div className="mx-auto mt-16 w-200">
+      <div className="mx-auto my-16 w-[95vw] sm:w-[90vw] sm:max-w-200 lg:w-200">
         <h1 className="text-title text-content-primary font-tight">Sua agenda</h1>
         <p className="text-paragraph-medium text-content-secondary">
           Aqui você pode ver todos os clientes e serviços agendados para hoje.
--- src/components/appointment-card/appointment-card.tsx
+++ src/components/appointment-card/appointment-card.tsx
@@ -13,22 +13,33 @@ export function AppointmentCard({ appointmentsArray }: AppointmentCardsProps) {
     );
   }
 
-  return appointmentsArray.map((appointment) => (
-    <li key={appointment.id} className="text-content-primary flex h-12 items-center justify-between px-3">
-      <span className="text-label-medium">
+  return appointmentsArray.map((appointment, index) => (
+    <li
+      key={appointment.id}
+      className={`text-content-primary flex h-fit flex-col items-center justify-between gap-0.5 px-1 py-4 sm:h-12
+        sm:flex-row sm:gap-4 sm:py-0 ${index === appointmentsArray.length - 1 ? "" : "border-b border-[#2E2C30]"}`}
+    >
+      <span className="text-label-medium flex w-full items-center gap-3 sm:w-fit">
         {appointment.scheduledAt.toLocaleTimeString("pt-BR", {
           hour: "2-digit",
           minute: "2-digit",
         })}
+        <span className="text-label-small w-fit text-center sm:hidden sm:w-full">
+          {appointment.petName}{" "}
+          <span className="text-content-secondary text-paragraph-small">/ {appointment.tutorName}</span>
+        </span>
       </span>
-      <span className="text-label-small w-full text-center">
+      <span className="text-label-small hidden w-full text-center sm:inline">
         {appointment.petName}{" "}
         <span className="text-content-secondary text-paragraph-small">/ {appointment.tutorName}</span>
       </span>
-      <span className="text-paragraph-small text-content-secondary w-full text-center">{appointment.description}</span>
+      <span className="text-paragraph-small text-content-secondary w-full text-left sm:text-center">
+        {appointment.description}
+      </span>
       <button
-        className="text-accent-red border-accent-red text-paragraph-small hover:bg-accent-red hover:text-content-primary
-          rounded-[5px] border px-2 py-1 transition-colors duration-200 ease-in-out"
+        className="text-accent-red border-accent-red/20 text-paragraph-small hover:bg-accent-red
+          hover:text-content-primary active:bg-accent-red active:text-content-primary mt-3 h-8 w-full cursor-pointer
+          rounded-[5px] border px-2 py-1 transition-colors duration-200 ease-in-out sm:mt-0 sm:h-fit sm:w-fit"
       >
         Remover
       </button>
